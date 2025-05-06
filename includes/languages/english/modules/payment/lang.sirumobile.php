5bc0b0109c8c9c8b091b78aa3640d4893c131f6b
İhsan Yüz <krbuk@nida.fi>
2025-05-06 14:10:40 +0300

    update
    update

---
+++ includes/languages/english/modules/payment/lang.sirumobile.php
@@ -0,0 +1,36 @@
+<?php
+$define = [
+	'MODULE_PAYMENT_SIRUMOBILE_TEXT_TITLE' => 'Sirumobile - Phone bill',
+	'MODULE_PAYMENT_SIRUMOBILE_TEXT_DESCRIPTION' => 'Sirumobile - ASETUKSET <br>Order Sirumobile using the form below. We process orders within one business day. Activating credit card payments requires company information and that the delivery, return and payment terms of the online store are in order
+		   <a href="https://www.Sirumobile.com" target="_blank">Get more information</a><br><br>
+		   <a href="https://merchant.Sirumobile.com" target="_blank">Log in to your Sirumobile account</a>',
+	'MODULE_PAYMENT_SIRUMOBILE_HEADER_ERROR' => 'Error!',
+	'MODULE_PAYMENT_SIRUMOBILE_TEXT_ERROR' => 'Error in Sirumobile payment!',
+	'MODULE_PAYMENT_SIRUMOBILE_ALERT_TEST' => 'Attention: test mode',
+	'MODULE_PAYMENT_SIRUMOBILE_ERROR' => 'Payment failed.',
+	'MODULE_PAYMENT_SIRUMOBILE_TEXT_API_ERROR' => '<b>ERROR</b> Sirumobile online payment -HMAC- signatures incorrect!',
+	'MODULE_PAYMENT_SIRUMOBILE_REWARD_POINT_TEXT' => 'Reward points',
+	'MODULE_PAYMENT_SIRUMOBILE_COUPON_TEXT' => 'Discount Coupons',
+	'MODULE_PAYMENT_SIRUMOBILE_GIFT_TEXT' => 'Gift Cards',
+	'MODULE_PAYMENT_SIRUMOBILE_LOWORDER_TEXT' => 'Small Order Extra',
+	'MODULE_PAYMENT_SIRUMOBILE_FREE_SHPING' => 'Free delivery',
+	'MODULE_PAYMENT_SIRUMOBILE_GROUP_TEXT' => 'Group discount',
+	'MODULE_PAYMENT_SIRUMOBILE_SUM_ROUND' => 'Sum round',
+	// Order more information
+	'MODULE_PAYMENT_SIRUMOBILE_SELECET_PAYMENT' => 'Choose payment method',
+	'MODULE_PAYMENT_SIRUMOBILE_TITLE_STATUS' => 'Sirumobile payment status : ', 
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_METHOD' => 'Payment method : ',
+	'MODULE_PAYMENT_SIRUMOBILE_REFERENCE_NUMBER' => 'Reference Number : ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_ERROR' => 'ERROR ! Sirumobile online payment service does not respond.',
+	'MODULE_PAYMENT_SIRUMOBILE_SELECET_OTHER' => 'Click the back button and choose another payment method',
+	// Payment information
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_OK' => '[Payment completed] ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_FAIL' => '[Payment status canceled] ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_PENDING' => '[Payment is not yet completed] ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_DELAYED' => '[Payment completed, requires approval on the Sirumobile Merchant Panel!] ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_NEW' => '[Payment has been created but nothing more] ',
+	'MODULE_PAYMENT_SIRUMOBILE_ORDER_NUMBER' => 'Order number : ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_AUTHRORIZED' => 'Payment verified.',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_SETTLED' => 'Payment charged.',
+];
+return $define;
---
+++ includes/languages/finnish/modules/payment/lang.sirumobile.php
@@ -0,0 +1,37 @@
+<?php
+$define = [
+	'MODULE_PAYMENT_SIRUMOBILE_TEXT_TITLE' => 'Sirumobile - Puhelinlaskulle',
+	'MODULE_PAYMENT_SIRUMOBILE_TEXT_DESCRIPTION' => 'Sirumobile - ASETUKSET <br>Tilaa Sirumobile oheisella lomakkeella. Käsittele tilaukset yhden arkipäivän kuluessa. Luottokorttimaksujen aktivoiminen edellyttää yrityksen tietoja sekä sitä, että verkkokaupan toimitus-, palautus- ja maksuehdot ovat kunnossa
+		   <a href="https://www.sirumobile.com" target="_blank">Hanki lisätietoja</a><br><br>
+		   <a href="https://merchant.Sirumobile.com" target="_blank">Kirjaudu Sirumobile-tiliisi</a>',
+	'MODULE_PAYMENT_SIRUMOBILE_HEADER_ERROR' => 'Virhe!',
+	'MODULE_PAYMENT_SIRUMOBILE_TEXT_ERROR' => 'Virhe Sirumobile -maksussa!',
+	'MODULE_PAYMENT_SIRUMOBILE_ALERT_TEST' => 'Huomio: Testitila',
+	'MODULE_PAYMENT_SIRUMOBILE_ERROR' => 'Maksu epäonnistui.',
+	'MODULE_PAYMENT_SIRUMOBILE_TEXT_API_ERROR' => '<b>VIRHE</b> Sirumobile verkkomaksu -HMAC-allekirjoituksiin väärin!',
+	'MODULE_PAYMENT_SIRUMOBILE_REWARD_POINT_TEXT' => 'Bonuspisteet',
+	'MODULE_PAYMENT_SIRUMOBILE_COUPON_TEXT' => 'Alennuskupongit',
+	'MODULE_PAYMENT_SIRUMOBILE_GIFT_TEXT' => 'Lahjakortit',
+	'MODULE_PAYMENT_SIRUMOBILE_LOWORDER_TEXT' => 'Pientilauslisä',
+	'MODULE_PAYMENT_SIRUMOBILE_FREE_SHPING' => 'Ilmainen toimitus',
+	'MODULE_PAYMENT_SIRUMOBILE_GROUP_TEXT' => 'Ryhmä alennus',
+	'MODULE_PAYMENT_SIRUMOBILE_SUM_ROUND' => 'Summa pyöreä',
+	// Order more information
+	'MODULE_PAYMENT_SIRUMOBILE_SELECET_PAYMENT' => 'Valitse maksutapa',
+	'MODULE_PAYMENT_SIRUMOBILE_TITLE_STATUS' => 'Sirumobile -maksun tila : ', 
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_METHOD' => 'Maksutapa : ',
+	'MODULE_PAYMENT_SIRUMOBILE_REFERENCE_NUMBER' => 'Viitenumero : ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_ERROR' => 'Virhe! Sirumobile verkkomaksupalvelu ei vastaa. ',
+	'MODULE_PAYMENT_SIRUMOBILE_SELECET_OTHER' => 'Kilikka takaisin nappi ja valitse muu maksutapa',
+	// Payment information
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_OK' => '[Maksu suoritettu] ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_FAIL' => '[Maksun tila peruttu] ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_PENDING' => '[Maksu ei vielä suoritettu] ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_DELAYED' => '[Maksu suoritettu, vaatii hyväksynnän Sirumobile kauppiaspaneeli!] ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_NEW' => '[Maksu on luotu, mutta ei mitään muuta] ',
+	'MODULE_PAYMENT_SIRUMOBILE_ORDER_NUMBER' => 'Tilausnumero : ',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_AUTHRORIZED' => 'Maksu varmennettu.',
+	'MODULE_PAYMENT_SIRUMOBILE_PAYMENT_SETTLED' => 'Maksu veloitettu.',
+];
+
+return $define;
